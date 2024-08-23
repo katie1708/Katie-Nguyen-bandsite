0a993b2e2eddd917c06a60b81e4ddc123fe2ea96
katie1708 <katienguyen3012@gmail.com>
2024-08-22 21:08:40 -0700

    Add tablet and desktop ver for Shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -34,7 +34,7 @@ let shows = [
 
 //Create shows item
 
-function createShowItem(show) {
+function createShowItemMobile(show) {
     //shows item as an article
     let showItem = document.createElement('article');
     showItem.classList.add('shows__item');
@@ -82,15 +82,101 @@ function createShowItem(show) {
     return showItem;
 }
 
+function createShowItemTablet(show) {
+    //shows item as an article
+    let showItem = document.createElement('article');
+    showItem.classList.add('shows__item');
+
+    //create item content from array
+    let dateInfo = document.createElement('p');
+    dateInfo.classList.add('shows__item-content');
+    dateInfo.classList.add('date');
+    dateInfo.innerText = show.date;
+
+    let venueInfo = document.createElement('p');
+    venueInfo.classList.add('shows__item-content');
+    venueInfo.innerText = show.venue;
+
+    let locationInfo = document.createElement('p');
+    locationInfo.classList.add('shows__item-content');
+    locationInfo.innerText = show.location;
+
+    //create button
+    let buyTicket = document.createElement('button');
+    buyTicket.innerText = "BUY TICKETS";
+
+    //add all elements to the item
+    showItem.appendChild(dateInfo);
+    showItem.appendChild(venueInfo);
+    showItem.appendChild(locationInfo);
+    showItem.appendChild(buyTicket);
+
+    return showItem;
+}
+
 //Render shows list
-function renderShow() {
+function renderShowMobile() {
     let showList = document.querySelector('.shows__list');
 
+    showList.innerHTML = "";
+
     for ( let i = 0 ; i < shows.length; i++ ) {
-        showItem = createShowItem(shows[i]);
+        showItem = createShowItemMobile(shows[i]);
         showList.appendChild(showItem);
 
     }
 }
 
-renderShow()
+function renderShowTablet() {
+    let showList = document.querySelector('.shows__list');
+
+    showList.innerHTML = "";
+
+    //create Date, Venue, Location headings 
+    let showHeading = document.createElement('div');
+    showHeading.classList.add('shows__heading');
+
+    let dateLabel = document.createElement('p');
+    dateLabel.classList.add('shows__item-label')
+    dateLabel.innerText = "DATE";
+
+    let venueLabel = document.createElement('p');
+    venueLabel.classList.add('shows__item-label')
+    venueLabel.innerText = "VENUE";
+
+    let locationLabel = document.createElement('p');
+    locationLabel.classList.add('shows__item-label')
+    locationLabel.innerText = "LOCATION";
+
+    showHeading.appendChild(dateLabel);
+    showHeading.appendChild(venueLabel);
+    showHeading.appendChild(locationLabel);
+
+    showList.appendChild(showHeading);
+
+    for ( let i = 0 ; i < shows.length; i++ ) {
+        showItem = createShowItemTablet(shows[i]);
+        showList.appendChild(showItem);
+
+    }
+}
+
+function renderShow() {
+    if (window.innerWidth < 980) {
+        renderShowMobile();
+    } else if (window.innerWidth >= 980) {
+        renderShowTablet();
+    } else;
+}
+
+renderShow();
+
+const screenSize = window.matchMedia("(min-width: 980px)");
+
+screenSize.addEventListener('change', (event) => {
+    if (event.matches) {
+        renderShowTablet();
+    } else {
+        renderShowMobile();
+    }
+  });
